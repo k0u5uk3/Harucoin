6150b6527f2e4ceae9c74ea0dbb8284639a9e952
k0u5uk3 <mail.to.k0u5uk3@gmail.com>
2015-08-10 01:16:54 +0900

    change unixepoch

--- src/main.cpp
+++ src/main.cpp
@@ -2010,13 +2010,13 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1317972665;
+        block.nTime    = 1439136741;
         block.nBits    = 0x1e0ffff0;
         block.nNonce   = 2084524493;
 
         if (fTestNet)
         {
-            block.nTime    = 1317798646;
+            block.nTime    = 1439136741;
             block.nNonce   = 385270584;
         }
 
