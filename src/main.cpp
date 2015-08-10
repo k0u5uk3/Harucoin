754d42c253d243a6fa3efba5b58242b0f39a68b2
k0u5uk3 <mail.to.k0u5uk3@gmail.com>
2015-08-10 10:53:15 +0900

    false generate genesisblock

--- src/main.cpp
+++ src/main.cpp
@@ -2027,7 +2027,7 @@ bool LoadBlockIndex(bool fAllowNew)
         assert(block.hashMerkleRoot == uint256("0xdcfb8e522c0a17c510434c74b0b534f857a0ffc2cad796953ff56a86c891f6d7"));
 
         // If genesis block hash does not match, then generate new genesis hash.
-        if (true && block.GetHash() != hashGenesisBlock)
+        if (false && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
             // This will figure out a valid hash and Nonce if you're
